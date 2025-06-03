3f17060c6bef784a08881ac01f5ea0aff1681c50
Daniel <emil.encalada@udla.edu.ec>
2025-06-03 07:46:30 -0500

    Se agrego la view tanco xaml y xaml.cs de chistes

--- Progreso2EncaladaD/Views/Chistes.xaml.cs
+++ Progreso2EncaladaD/Views/Chistes.xaml.cs
@@ -1,9 +1,35 @@
+using Progreso2EncaladaD.Repository;
+
 namespace Progreso2EncaladaD.Views;
 
 public partial class Chistes : ContentPage
 {
-	public Chistes()
-	{
+	private readonly ChistesRepository _chistesRepository;
+
+	public Chistes() 
+	{ 
 		InitializeComponent();
+		_chistesRepository = new ChistesRepository();
+		CargarChiste();
 	}
+
+	private async void CargarChiste() 
+	{
+		try
+		{
+			var chiste = await _chistesRepository.GetChisteAsync();
+			ChisteLabel.Text = chiste;
+		}
+		catch (Exception ex)
+		{
+			ChisteLabel.Text = $"Error en la carga del chiste: {ex.Message}";
+		}	
+	}
+
+
+	private void OtroChisteClicked(object sender, EventArgs e) 
+	{
+		CargarChiste();
+	}
+
 }
